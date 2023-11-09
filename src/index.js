9fa20a7e06d1ed982530fd522401b1048ad7495e
vitaZ-dev <vitaminzdev@gmail.com>
2023-11-09 13:40:00 +0900

    feat: vanillaJs counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello')
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const number = document.querySelector('span');
+let count = 0;
+number.innerText = count;
+const handleAdd = () => {
+  console.log('plus');
+  count = count + 1;
+  number.innerText = count;
+}
+const handleMinus = () => {
+  console.log('minus');
+  count = count - 1;
+  number.innerText = count;
+}
+plus.addEventListener('click', handleAdd)
+minus.addEventListener('click', handleMinus)
