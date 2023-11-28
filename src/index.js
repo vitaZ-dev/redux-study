b2d326ae87ab37c4d0a8c30e20b949247c0ec0d8
vitaZ-dev <vitaminzdev@gmail.com>
2023-11-28 21:39:28 +0900

    feat: actions

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ const plus = document.querySelector('.plus');
 const minus = document.querySelector('.minus');
 const number = document.querySelector('span');
 
-const countModifier = (count = 0) => {
-  // modify state
-  return count;
+const countModifier = (count = 0, action) => {
+  // console.log(action);
+  if (action.type === "add") {
+    console.log('add count');
+    return count + 1;
+  } else if (action.type === 'minus') {
+    console.log('minus count');
+    return count - 1;
+  } else {
+    return count;
+  }
 };
+
 const countStore = createStore(countModifier);
 
-console.log(countStore);
-console.log(countStore.getState());
+countStore.dispatch({type: "add"});
+countStore.dispatch({type: "add"});
+countStore.dispatch({type: "add"});
+countStore.dispatch({type: "minus"});
+
+console.log(countStore.getState())
