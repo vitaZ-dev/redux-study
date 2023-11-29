0a44a2af5ce2b5ece69e3803aef2e1f64ab809e6
vitaZ-dev <vitaminzdev@gmail.com>
2023-11-29 18:18:03 +0900

    refactor: code refactoring

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const minus = document.querySelector('.minus');
 const number = document.querySelector('span');
 
 const countModifier = (count = 0, action) => {
-  // console.log(action);
-  if (action.type === "add") {
-    return count + 1;
-  } else if (action.type === 'minus') {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case "add":
+      return count + 1;
+    case "minus":
+      return count - 1;
+    default:
+      return count;
   }
 };
 
