7552df1e8388dd5b1fb6d4fde64d5960ef5170fa
vitaZ-dev <vitaminzdev@gmail.com>
2023-11-30 00:08:55 +0900

    feat: vanilla todo - redux setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+import { createStore } from "redux";
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
 const $form = document.querySelector('form');
 const $input = document.querySelector('input');
 const $ul = document.querySelector('ul');
-
-const createTodo = (todo) => {
-  const li = document.createElement('li');
-  li.innerText = todo;
-  $ul.appendChild(li);
-}
 
 const onSubmit = (e) => {
   e.preventDefault();
   const todo = $input.value;
   $input.value = '';
-  createTodo(todo);
+  store.dispatch({ type: ADD_TODO, text: todo });
 }
 $form.addEventListener('submit', onSubmit);
