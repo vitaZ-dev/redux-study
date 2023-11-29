0df58a1883da57d689ea2c274114cbe38e92f112
vitaZ-dev <vitaminzdev@gmail.com>
2023-11-29 22:41:38 +0900

    feat: vanilla todo - dataless

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-import { createStore } from 'redux';
+const $form = document.querySelector('form');
+const $input = document.querySelector('input');
+const $ul = document.querySelector('ul');
 
-const plus = document.querySelector('.plus');
-const minus = document.querySelector('.minus');
-const number = document.querySelector('span');
-
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case "add":
-      return count + 1;
-    case "minus":
-      return count - 1;
-    default:
-      return count;
-  }
-};
-
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
+const createTodo = (todo) => {
+  const li = document.createElement('li');
+  li.innerText = todo;
+  $ul.appendChild(li);
 }
-countStore.subscribe(onChange)
 
-plus.addEventListener('click', () => { countStore.dispatch({type: "add"}) });
-minus.addEventListener('click', () => { countStore.dispatch({type: "minus"}) });
+const onSubmit = (e) => {
+  e.preventDefault();
+  const todo = $input.value;
+  $input.value = '';
+  createTodo(todo);
+}
+$form.addEventListener('submit', onSubmit);
