dcab6d4ba1a7c59d9f6ffbb935b27d3ece46251b
vitaZ-dev <vitaminzdev@gmail.com>
2023-11-29 18:15:05 +0900

    feat: Subscriptions

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ const number = document.querySelector('span');
 const countModifier = (count = 0, action) => {
   // console.log(action);
   if (action.type === "add") {
-    console.log('add count');
     return count + 1;
   } else if (action.type === 'minus') {
-    console.log('minus count');
     return count - 1;
   } else {
     return count;
@@ -19,9 +17,10 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type: "add"});
-countStore.dispatch({type: "add"});
-countStore.dispatch({type: "add"});
-countStore.dispatch({type: "minus"});
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange)
 
-console.log(countStore.getState())
+plus.addEventListener('click', () => { countStore.dispatch({type: "add"}) });
+minus.addEventListener('click', () => { countStore.dispatch({type: "minus"}) });
