f54de9661f2615d17c3e757d755033437ce4c48e
vitaZ-dev <vitaminzdev@gmail.com>
2023-12-05 16:51:16 +0900

    redux toolkit - configureStore

--- src/store.js
+++ src/store.js
@@ -1,21 +1,7 @@
-import { createStore } from 'redux';
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
-
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addTodo.type:
-//       console.log(action);
-//       return [{text: action.payload, id: Date.now()}, ...state];
-//     case deleteTodo.type:
-//       console.log(action);
-//       return state.filter(todo => todo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// }
 
 const reducer = createReducer([], (builder) => {
   builder
@@ -27,7 +13,7 @@ const reducer = createReducer([], (builder) => {
   })
 })
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export const actionCreators = { addTodo, deleteTodo };
 export default store;
